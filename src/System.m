6431cb29c8a41bcd9f08a40b14a2834297ad47ab
imadat <tatsumi.imada@ist.osaka-u.ac.jp>
2026-06-23 06:49:25 +0900

    fix: update version info

--- src/System.m
+++ src/System.m
@@ -14,7 +14,7 @@ classdef System < handle
             % GETCURRENTVERSION Returns the version of this application build.
             % Update this value when creating a new release tag.
 
-            version = "2.0.0";
+            version = "2.2.0";
 
         end % function version = getCurrentVersion()
 
