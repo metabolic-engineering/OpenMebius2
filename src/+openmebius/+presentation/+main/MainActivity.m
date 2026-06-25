1529f9466661dfa9341d2bbfae79a0f86c24ac64
imadat <tatsumi.imada@ist.osaka-u.ac.jp>
2026-06-26 06:52:28 +0900

    feat: Remove Completed state from MainActivity and add MainUIPolicy class for UI evaluation logic

--- src/+openmebius/+presentation/+main/MainActivity.m
+++ src/+openmebius/+presentation/+main/MainActivity.m
@@ -4,7 +4,6 @@ classdef MainActivity
         Idle
         Busy
         Running
-        Completed
     end
 
 end
---
+++ src/+openmebius/+presentation/+main/MainUIPolicy.m
@@ -0,0 +1,94 @@
+classdef MainUIPolicy
+
+    methods (Static)
+
+        function ui = evaluate(context, state)
+
+            arguments
+                context struct
+                state openmebius.presentation.main.MainPresentationState
+            end
+
+            import openmebius.presentation.main.MainActivity
+            import openmebius.presentation.main.EditTarget
+
+            isIdle = state.Activity == MainActivity.Idle;
+            isBusy = state.Activity == MainActivity.Busy;
+            isRunning = state.Activity == MainActivity.Running;
+
+            isEditing = state.EditTarget ~= EditTarget.None;
+
+            ui = struct();
+
+            % Project
+            ui.ProjectBrowseEnabled = isIdle && ~isEditing;
+            ui.ProjectLoadEnabled = isIdle && ~isEditing;
+            ui.ProjectMetadataEditable = ...
+                isIdle && context.HasProject && ~isEditing;
+            ui.ProjectSaveEnabled = ...
+                isIdle && context.HasProject && ~isEditing;
+
+            % Model
+            ui.ModelEnabled = ...
+                context.HasProject && ~isBusy && ~isRunning;
+
+            ui.ModelEditEnabled = ...
+                isIdle && context.HasModel && ~isEditing;
+
+            ui.ModelTableEditable = ...
+                isIdle && ...
+                state.EditTarget == EditTarget.Model;
+
+            ui.ModelSaveEnabled = ui.ModelTableEditable;
+
+            % MS
+            ui.MsEnabled = ...
+                context.HasModel && ~isBusy && ~isRunning;
+
+            ui.MsTableEditable = ...
+                isIdle && ...
+                state.EditTarget == EditTarget.MassSpectrometry;
+
+            % Experiment
+            ui.ExperimentEnabled = ...
+                context.HasModel && ~isBusy && ~isRunning;
+
+            ui.ExperimentTableEditable = ...
+                isIdle && ...
+                state.EditTarget == EditTarget.Experiment;
+
+            % Tracer
+            ui.TracerEnabled = ...
+                context.HasExperiments && ~isBusy && ~isRunning;
+
+            ui.TracerTableEditable = ...
+                isIdle && ...
+                state.EditTarget == EditTarget.Tracer;
+
+            % Run
+            ui.RunConfigurationEnabled = ...
+                isIdle && context.HasBatches && ~isEditing;
+
+            ui.RunButtonEnabled = ...
+                (isIdle && context.CanRun && ~isEditing) || isRunning;
+
+            if isRunning
+                ui.RunButtonText = "Cancel";
+            else
+                ui.RunButtonText = "Run";
+            end
+
+            % Result
+            ui.ResultEnabled = ...
+                context.HasResults && ~isBusy;
+
+            ui.ResultEditable = false;
+
+            % Global indication
+            ui.IsBusy = isBusy;
+            ui.IsRunning = isRunning;
+        end
+
+    end
+
+end
