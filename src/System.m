00ec41898816bf4c282834df0c8c013a09ba9e98
imadat <tatsumi.imada@ist.osaka-u.ac.jp>
2026-08-07 18:07:59 +0900

    feat: update version to 3.1.1 in splash assets and System class
    - Changed version text in splash.svg from "3.1.0" to "3.1.1".
    - Updated splash.png binary file.
    - Modified System.m to return version "3.1.1" in getCurrentVersion method.

--- src/System.m
+++ src/System.m
@@ -14,7 +14,7 @@ classdef System < handle
             % GETCURRENTVERSION Returns the version of this application build.
             % Update this value when creating a new release tag.
 
-            version = "3.1.0";
+            version = "3.1.1";
 
         end % function version = getCurrentVersion()
 
